f6f7afa81a714ebd768736ce76801da9e7decb42
Conrad Storz <conradstorz@gmail.com>
2023-01-22 21:12:09 -0500

    split functionality to sub modules

---
+++ LCD_statusd.py
@@ -0,0 +1,47 @@
+"""Initialize LCD module and display status.
+local IP address
+Number of devices online
+"""
+
+
+"""
+from subprocess import check_output
+lcd.message('Local IP Address:\n')
+lcd.message(ips2 = check_output(['hostname', '--all-ip-addresses']).decode('utf-8'))
+
+read JSON file that has IDs and locations and last seen info
+determine how many devices are currently active
+active_devices = 0
+"""
+import time
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+# Modify this if you have a different sized Character LCD
+LCD_COLUMNS = 16
+LCD_ROWS = 2
+RED_BACKLIGHT = [100, 0, 0]
+BLUE_BACKLIGHT = [0, 100, 0]
+GREEN_BACKLIGHT = [0, 0, 100]
+PURPLE_BACKLIGHT = [50, 0, 50]
+BACKLIGHT_OFF = [0, 0, 0]
+WELCOME_MESSAGE = "Hello\nCircuitPython"
+EXIT_MESSAGE = "Going to sleep\nCya later!"
+
+# Initialise I2C bus.
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+# Initialise the LCD class
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, LCD_COLUMNS, LCD_ROWS)
+
+
+def start_LCD_daemon():
+    """Primary function"""
+    lcd.clear()
+    # Set LCD color to red
+    lcd.color = RED_BACKLIGHT
+    # Print two line message
+    lcd.message = WELCOME_MESSAGE
+    time.sleep(.1)
+    lcd.message = "IP Address\nUNKNOWN"
+    return
---
+++ weather_underground.py
@@ -0,0 +1,31 @@
+""" TODO get outside temperature and humidity from open weather map and place into csv database with each reading of sensors.
+TODO note that once per 12 minutes is probably frequent enough to check temps and humidity
+Thank you for subscribing to Free OpenWeatherMap!
+
+API key:
+- Your API key is 5a51770d8fa87227c5c1a07f3f9240fd
+- Within the next couple of hours, it will be activated and ready to use
+- You can later create more API keys on your account page
+- Please, always use your API key in each API call
+
+Endpoint:
+- Please, use the endpoint api.openweathermap.org for your API calls
+- Example of API call:
+api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5a51770d8fa87227c5c1a07f3f9240fd
+
+lat = '38.317139'
+lon = '-85.868167'
+API = '5a51770d8fa87227c5c1a07f3f9240fd'
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API}"
+
+Useful links:
+- API documentation https://openweathermap.org/api
+- Details of your plan https://openweathermap.org/price
+- Please, note that 16-days daily forecast and History API are not available for Free subscribers
+"""
+
+def get_temp_and_humidity():
+    """Contact weather underground and return a tuple of temperature for given zipcode and humidity."""
+    temp = 0
+    humid = 0
+    return (temp, humid)
