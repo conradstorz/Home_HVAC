7d393dfc47a95c863f41c985cc81b8abd4cf2c81
Conrad Storz <conradstorz@gmail.com>
2023-02-03 10:32:17 -0500

    added documentation comment

--- EntroPi_project/src/EntroPi/csv_functions.py
+++ EntroPi_project/src/EntroPi/csv_functions.py
@@ -14,6 +14,9 @@ def generate_csv_filename(basename=None):
 
 @logger.catch
 def write_csv(device_data, directory="."):
+    """Takes a dict and an optional directory= arguments and writes to a file
+        with todays date as part of the filename. Each day the old filename
+        is abandoned and a new filename is started."""
     # Open the output CSV file and write 'the results
     # print(f"\nBegin saving data...")
     # print(f"\ndevice_data:\n{device_data}\n")
