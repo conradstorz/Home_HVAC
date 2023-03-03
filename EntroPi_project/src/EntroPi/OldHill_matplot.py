9851873e4cfbdc7875b64026e9c7b9128a5f04fc
Conrad Storz <conradstorz@gmail.com>
2023-03-03 10:56:26 -0500

    asked ChatGPT to help expand functionality

--- EntroPi_project/src/EntroPi/OldHill_matplot.py
+++ EntroPi_project/src/EntroPi/OldHill_matplot.py
@@ -10,7 +10,108 @@ def main():
     # Open the CSV file and read the contents
     with open("20230122_HVAC_temps.csv", "r") as f:
         # TODO make compat with zipped csv
+        """03-02-2023 ChatGPT suggested code for dealing with zip files and multiple CSV files inside or outside of a zip.
+        import glob
+        import zipfile
+        import io
+
+        # Find all the CSV files in the current directory, including those in zip files
+        csv_files = []
+        for file in glob.glob("*"):
+            if file.endswith(".csv"):
+                csv_files.append(file)
+            elif file.endswith(".zip"):
+                with zipfile.ZipFile(file, 'r') as zip:
+                    for zip_file in zip.namelist():
+                        if zip_file.endswith(".csv"):
+                            csv_files.append(f"{file}!{zip_file}")
+
+        # Print out the list of CSV files
+        print('Available files:')
+        for i, file in enumerate(csv_files):
+            print(f'{i+1}. {file}')
+
+        # Prompt the user to select a file
+        while True:
+            try:
+                selection = int(input('Enter the number of the file you want to use: '))
+                if selection < 1 or selection > len(csv_files):
+                    raise ValueError
+                break
+            except ValueError:
+                print(f'Invalid selection. Please enter a number between 1 and {len(csv_files)}')
+
+        # Get the selected file name and, if it's in a zip file, decompress it to a temporary file
+        selected_file = csv_files[selection-1]
+        if "!" in selected_file:
+            zip_file, csv_file = selected_file.split("!")
+            with zipfile.ZipFile(zip_file, 'r') as zip:
+                with zip.open(csv_file) as csv:
+                    temp_file = io.StringIO(csv.read().decode())
+        else:
+            temp_file = open(selected_file, 'r')
+
+        # Now you can use the temp_file variable to read the CSV data and plot the graph
+
+        # Once you're done, make sure to close the temp_file
+        temp_file.close()
+
+        """
         # TODO make interactive to choose from several
+        """ChatGPT suggested code for when there are too many files to reasonably display.
+        import re
+        import os
+        import zipfile
+        import io
+
+        csv_files = []
+
+        # Find all the CSV files in the current directory
+        for file in os.listdir():
+            if file.endswith(".csv"):
+                csv_files.append(file)
+            elif file.endswith(".zip"):
+                with zipfile.ZipFile(file, 'r') as zip:
+                    for zip_file in zip.namelist():
+                        if zip_file.endswith(".csv"):
+                            csv_files.append(f"{file}!{zip_file}")
+
+        # Prompt the user to enter a search query
+        print("Enter a search query to find the file you want to plot.")
+        while True:
+            search_query = input("Search: ")
+            # Sanitize the search query
+            search_query = re.sub(r"[^a-zA-Z0-9_ ]", "", search_query)
+            if search_query:
+                break
+            else:
+                print("Invalid search query. Please try again.")
+
+        # Filter the list of CSV files based on the search query
+        filtered_csv_files = [f for f in csv_files if search_query in f]
+        if not filtered_csv_files:
+            print("No matching files found.")
+            exit()
+        elif len(filtered_csv_files) == 1:
+            file = filtered_csv_files[0]
+        else:
+            # Display the list of matching CSV files to the user
+            print("Matching files:")
+            for i, file in enumerate(filtered_csv_files):
+                print(f"{i+1}. {file}")
+            # Prompt the user to choose a file
+            while True:
+                choice = input("Enter the number of the file you want to plot (q to quit): ")
+                if choice == "q":
+                    exit()
+                try:
+                    index = int(choice) - 1
+                    file = filtered_csv_files[index]
+                    break
+                except (ValueError, IndexError):
+                    print("Invalid choice. Please try again.")
+    
+        """
         # TODO make to choose most recent from directory automatically
         reader = csv.DictReader(f)
         # Iterate through the rows of the CSV file
