59cd636a6355f8bc626bed7575b136011a26c441
Conrad Storz <conradstorz@gmail.com>
2023-03-19 13:14:26 -0400

    corrected display of dual ip addresses and no addr

--- EntroPi_project/src/EntroPi/LCD_statusd.py
+++ EntroPi_project/src/EntroPi/LCD_statusd.py
@@ -43,15 +43,17 @@ def start_LCD_daemon():
     """Periodically update attached LCD with details of running program.
     This code exits quickly but could be long running if it is compatible
     with some type of hypervisor multitasking scheme."""
+    # TODO add monitoring of internet availability and log status
     logger.info("Start LCD update.")
     lcd.clear()
     # Set LCD color to red
     lcd.color = RED_BACKLIGHT
     # Print two line message
     lcd.message = WELCOME_MESSAGE
-    time.sleep(0.1)
+    time.sleep(0.2)
     lcd.clear()
     lcd.message = "IP Address\nUNKNOWN"
+    time.sleep(1)
     ips2 = check_output(["hostname", "--all-ip-addresses"]).decode("utf-8").split()
     # TODO check that IP address is valid
     logger.info(f"IP address lookup: {ips2}")
@@ -59,7 +61,10 @@ def start_LCD_daemon():
     # Set LCD color to red
     lcd.color = GREEN_BACKLIGHT
     # Print two line message
-    ip1 = ips2[0]
+    if len(ips2) > 0:
+        ip1 = ips2[0]
+    else:
+        ip1 = "None"
     if len(ips2) > 1:
         ip2 = ips2[1]
     else:
